846d2e3e0253dc017da433275976f2a5b41db5f1
Anelia Peycheva <anelia.peicheva@gmail.com>
2024-09-24 15:53:29 +0300

    ASP.NET Core Introduction Exercise

---
+++ 01. ASP.NET Core Introduction/CinemaApp.Data/CinemaDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace CinemaApp.Data
+{
+    using System.Reflection;
+    using Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext()
+        {
+            
+        }
+
+        public CinemaDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
